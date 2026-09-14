8f7345246c113354e404abac060540296a691a2f
niketangoyal-jtg <niketan.goyal@joshtechnologygroup.com>
2026-09-14 10:52:32 +0530

    [NG_A1_00]: fix the route for home

--- src/script/index.js
+++ src/script/index.js
@@ -1,9 +1,16 @@
 const links = document.querySelectorAll('.header__link');
 
-links.forEach((element) => {
-    element.addEventListener('click', () => {
-        let prevactive = document.querySelector('.header__link--active');
-        if (prevactive) prevactive.classList.toggle('header__link--active');
-        element.classList.toggle('header__link--active');
+function updateActiveLink() {
+    const currentHash = window.location.hash;
+    links.forEach((element) => {
+        element.classList.toggle(
+            'header__link--active',
+            element.getAttribute('href') === currentHash ||
+                (!currentHash && element.getAttribute('href') === '/'),
+        );
     });
-});
+}
+
+updateActiveLink();
+
+window.addEventListener('hashchange', updateActiveLink);
